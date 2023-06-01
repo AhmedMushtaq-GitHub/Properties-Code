6e9d9d565fb386987c074a9a552394bdfea05f92
AhmedMushtaq-GitHub <ahmed@AHMEDMUSHTAQ>
2023-06-01 18:02:53 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp1;
+
+//Long type Property
+Student st = new Student();
+Console.WriteLine("Please Enter name");//Message to user
+st.Name = Console.ReadLine()??"";//saving user input
+
+//Auto type Property
+st.Description = ",Your wellcome Our web Site.Tell me do you have experions about csharp";
+Console.WriteLine($"\nOk Dear {st.Name} {st.Description}");//Output
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        private string name;
+        private string description;
+        
+        //Long type Property
+        public string Name
+        {
+            get 
+            { 
+                return name; 
+            }
+            set 
+            {
+                name = value;
+            }
+                
+        }
+        //Auto type Property
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
